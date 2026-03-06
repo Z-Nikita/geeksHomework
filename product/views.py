2a7ec54023bc29fdfa6baf048fc6e9e92e410bdd
Z-Nikita <nik1210022@gmail.com>
2026-03-06 21:14:31 +0600

    Finished hw3

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Avg, Count, Value
 from django.db.models.functions import Coalesce
 from rest_framework import generics
+
 from .models import Category, Product, Review
 from .serializers import (
     CategorySerializer,
@@ -10,33 +11,29 @@ from .serializers import (
 )
 
 
-# Categories
-class CategoryListAPIView(generics.ListAPIView):
+class CategoryListAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.annotate(products_count=Count("products", distinct=True)).all()
     serializer_class = CategorySerializer
 
 
-class CategoryDetailAPIView(generics.RetrieveAPIView):
+class CategoryDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.annotate(products_count=Count("products", distinct=True)).all()
     serializer_class = CategorySerializer
     lookup_field = "id"
 
 
-# Products
-class ProductListAPIView(generics.ListAPIView):
+class ProductListAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.select_related("category").all()
     serializer_class = ProductSerializer
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.select_related("category").all()
     serializer_class = ProductSerializer
     lookup_field = "id"
 
 
 class ProductReviewsListAPIView(generics.ListAPIView):
-    """List products with nested reviews + average rating (Avg stars)."""
-
     queryset = (
         Product.objects.select_related("category")
         .prefetch_related("reviews")
@@ -46,13 +43,12 @@ class ProductReviewsListAPIView(generics.ListAPIView):
     serializer_class = ProductWithReviewsSerializer
 
 
-# Reviews
-class ReviewListAPIView(generics.ListAPIView):
+class ReviewListAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.select_related("product").all()
     serializer_class = ReviewSerializer
 
 
-class ReviewDetailAPIView(generics.RetrieveAPIView):
+class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.select_related("product").all()
     serializer_class = ReviewSerializer
     lookup_field = "id"
